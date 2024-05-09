f832343460a9865939ba32d770b98cb156246644
KrypticGadget <126850454+KrypticGadget@users.noreply.github.com>
2024-05-08 17:00:57 -0700

    Update file_server.py

--- file_server.py
+++ file_server.py
@@ -1,10 +1,16 @@
+import os
 from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
+# Use an environment variable to define the path, or default to a relative path
+DATA_DIR = os.getenv('DATA_DIRECTORY', 'data')
+
 @app.route('/logs/<path:filename>')
 def download_file(filename):
-    return send_from_directory('C:\\Users\\krypt\\OneDrive\\Desktop\\Cleric AI\\V2\\Code\\P7\\data', filename)
+    return send_from_directory(DATA_DIR, filename)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    # Bind to PORT if defined, default to 5000
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
